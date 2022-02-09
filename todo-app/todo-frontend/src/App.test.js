faaa09a0e35472dd717d768f0c72232de6dc25c3
Mickey <57460035+coreAux@users.noreply.github.com>
2022-02-09 15:06:08 +0100

    part 12 ex 13 to 14

--- todo-app/todo-frontend/src/App.test.js
+++ todo-app/todo-frontend/src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Todo from './Todos/Todo';
 
-test('renders learn react link', () => {
+test('renders title Todos', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Todos/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+const todo = { text: "This is something to do...", done: false }
+
+const onClickDelete = jest.fn()
+const onClickComplete = jest.fn()
+
+test('exercise 12.14', () => {
+
+  const component = render(
+    <Todo
+      todo={todo}
+      onClickDelete={onClickDelete}
+      onClickComplete={onClickComplete}
+    />
+  )
+
+  expect(component.container).toHaveTextContent("This is something to do...")
+  expect(component.container).not.toHaveTextContent("It's all wrong!")
+})
